f3741d37e44bbec8ce8320a2f397cff3ac9d8170
Rohya <rohit.thorawade51@gmail.com>
2020-01-27 15:02:44 +0530

    modified: pom.xml

--- src/main/java/com/bridgelabz/fb/base/TestBase.java
+++ src/main/java/com/bridgelabz/fb/base/TestBase.java
@@ -43,16 +43,15 @@ public class TestBase {
 	}
 
 	public void intialization() {
-		String browsername = prop.getProperty("browser");
+		String browsername = prop.getProperty("browser");		
 		if (browsername.equalsIgnoreCase("chrome")) {
 
 			System.setProperty("webdriver.chrome.driver",
 					"/home/admin-1/eclipse-workspace/FacebookAutomation/driver/chromedriver");
-               
-
 			ChromeOptions option = new ChromeOptions();
 			option.addArguments("--disable-notifications");
 			driver = new ChromeDriver(option);
+			
 		}
 		driver.manage().window().maximize();
 		driver.manage().deleteAllCookies();
